78c4233862f5946809e9885653db9e5a0eb124c7
Elliot Chance <elliotchance@gmail.com>
2012-03-28 19:44:29 +1100

    Initial commit.

---
+++ src/org/z/Main.java
@@ -0,0 +1,12 @@
+package org.z;
+
+public class Main {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+	
+}
